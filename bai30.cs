b13fa26b8d7625a2e4c1c377cec91f627c81d69d
truongle05 <lequangtruong17082005@gmail.com>
2024-07-04 17:41:41 +0700

    Add files via upload

---
+++ bai30.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text;
+
+namespace Bai28_Chuong6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            class_demo ob = new class_demo();
+            ob.Show();
+
+            ushort n;
+            Console.WriteLine("Nhập n:");
+            n = ob.nhapsonguyen2bytekhongdau();
+            Console.WriteLine("n = " + n.ToString());
+        }
+    }
+}
